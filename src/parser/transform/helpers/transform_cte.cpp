aa2163c12ee9a51fd0071ee43795d615b85cf83e
yiqiao <yimingqiao3163@gmail.com>
2026-04-08 00:25:22 +0800

    Revert default WITH materialization change

--- src/parser/transform/helpers/transform_cte.cpp
+++ src/parser/transform/helpers/transform_cte.cpp
@@ -109,11 +109,11 @@ void Transformer::TransformCTE(duckdb_libpgquery::PGWithClause &de_with_clause, 
 			throw ParserException("Duplicate CTE name \"%s\"", cte_name);
 		}
 
-			if (cte.ctematerialized == duckdb_libpgquery::PGCTEMaterializeDefault) {
-				info->materialized = CTEMaterialize::CTE_MATERIALIZE_ALWAYS;
-			} else if (cte.ctematerialized == duckdb_libpgquery::PGCTEMaterializeAlways) {
-				info->materialized = CTEMaterialize::CTE_MATERIALIZE_ALWAYS;
-			} else if (cte.ctematerialized == duckdb_libpgquery::PGCTEMaterializeNever) {
+		if (cte.ctematerialized == duckdb_libpgquery::PGCTEMaterializeDefault) {
+			info->materialized = CTEMaterialize::CTE_MATERIALIZE_DEFAULT;
+		} else if (cte.ctematerialized == duckdb_libpgquery::PGCTEMaterializeAlways) {
+			info->materialized = CTEMaterialize::CTE_MATERIALIZE_ALWAYS;
+		} else if (cte.ctematerialized == duckdb_libpgquery::PGCTEMaterializeNever) {
 			info->materialized = CTEMaterialize::CTE_MATERIALIZE_NEVER;
 		}
 
